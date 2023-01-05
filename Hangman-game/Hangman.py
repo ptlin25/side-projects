4951e27513aac69553a093a14bbfd48113c3eca9
Patrick Lin <patricklin@Patricks-MacBook-Pro.local>
2023-01-04 22:09:43 -0600

    New project directory

---
+++ Hangman-game/Hangman.py
@@ -0,0 +1,80 @@
+class Hangman():    
+    def __init__(self, word):
+        self.word = word
+        self.revealed = ["_" for _ in word]
+        self.stage = 0
+    
+    def guess(self, letter):
+        images = [
+        """
+        +---+
+            |
+            |
+            |
+            |
+          =====
+        """, 
+        """
+        +---+
+        O   |
+            |
+            |
+            |
+          =====
+        """,
+        """
+        +---+
+        O   |
+        |   |
+            |
+            |
+          =====
+        """,
+        """
+        +---+
+        O   |
+       /|   |
+            |
+            |
+          =====
+        """,
+        """
+        +---+
+        O   |
+       /|\  |
+            |
+            |
+          =====
+        """,
+        """
+        +---+
+        O   |
+       /|\  |
+       /    |
+            |
+          =====
+        """,
+        """
+        +---+
+        O   |
+       /|\  |
+       / \  |
+            |
+          =====
+        """
+        ]
+
+        wrong = True
+        for i, char in enumerate(self.revealed):
+            if char == letter:
+                self.revealed[i] == letter
+                wrong = False
+
+        if wrong:
+            self.stage += 1
+        
+        print(images[self.stage])
+        print("".join(self.revealed))
+    
+    def is_over(self):
+        return self.stage == 6 or self.revealed == self.word
