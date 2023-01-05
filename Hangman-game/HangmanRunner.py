1958517866d6409b1ea5d9cd0d170ddb18885e88
Patrick Lin <patricklin@Patricks-MacBook-Pro.local>
2023-01-04 22:16:38 -0600

    Runner added

---
+++ Hangman-game/HangmanRunner.py
@@ -0,0 +1,12 @@
+from Hangman import Hangman
+
+class HangmanRunner():
+    def main():
+        word = input("Enter the secret word. ")
+        game = Hangman(word.lower)
+        while not game.is_over:
+            letter = input("Guess a letter. ")
+            game.guess(letter)
+            
+    if __name__ == "__main__":
+        main()
