53099ece5fb1f46d7dc809bcf86c5b695c291adf
Patrick Lin <patricklin@Patricks-MacBook-Pro.local>
2023-01-14 22:30:42 -0600

    New text

--- Hangman-game/Hangman.py
+++ Hangman-game/Hangman.py
@@ -1,8 +1,11 @@
 class Hangman():    
     def __init__(self, word):
+        if not word.isalpha():
+            raise Exception("Invalid word.")
         self.word = word.lower()
         self.revealed = ["_"] * len(word)
         self.stage = 0
+        self.guessed = set()
     
     def guess(self, letter):
         images = [
@@ -64,17 +67,24 @@ class Hangman():
         """
         ]
 
-        wrong = True
-        for i, char in enumerate(self.word):
-            if char == letter:
-                self.revealed[i] = char
-                wrong = False
+        letter = letter.lower()
+        if not letter.isalpha() or len(letter) != 1:
+            print("Please guess a letter")
+        elif letter in self.guessed:
+            print("You have already guessed this letter.")
+        else:
+            self.guessed.add(letter)
+            wrong = True
+            for i, char in enumerate(self.word):
+                if char == letter:
+                    self.revealed[i] = char
+                    wrong = False
 
-        if wrong:
-            self.stage += 1
+            if wrong:
+                self.stage += 1
         
-        print(images[self.stage])
-        print(self.revealed)
+            print(images[self.stage])
+            print(self.revealed)
     
     def is_over(self):
         return self.stage == 6 or "".join(self.revealed) == self.word
--- Hangman-game/HangmanRunner.py
+++ Hangman-game/HangmanRunner.py
@@ -2,18 +2,20 @@ from Hangman import Hangman
 
 class HangmanRunner():
     def main():
-        word = input("Enter the secret word. ")
+        word = input("Enter the secret word. \n")
         
         game = Hangman(word)
         print(game.revealed)
         
         
         while not game.is_over():
-            g = input("Guess a letter.")
+            g = input("Guess a different letter. \n")
             game.guess(g)
-        
-        
-        
-            
+
+        if game.word == "".join(game.revealed):
+            print("YOU WIN!")
+        else:
+            print("GAME OVER!")
+
     if __name__ == "__main__":
         main()
